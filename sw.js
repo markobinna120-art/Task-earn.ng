3507d65c1f268a2fca97d2ed6f487f22cf125f6e
markobinna120-art <markobinna120@gmail.com>
2026-09-23 22:45:04 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,6 @@
-
+// Online only - no offline cache
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
+});
